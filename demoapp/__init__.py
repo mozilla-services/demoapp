764a7ec3327a761f13245a0a83f27cb6d5635063
Tarek Ziadé <tarek@mozilla.com>
2011-10-07 15:31:46 +0200

    externalized the url matching

--- demoapp/__init__.py
+++ demoapp/__init__.py
@@ -3,6 +3,14 @@ from demoapp.resources import Root
 from demoapp.config import Config
 
 
+# add your functions here
+URLS = (
+ ('/', 'demoapp.views.default_view', 'json'),
+ ('/config', 'demoapp.views.default_view', 'json'),
+)
+
+
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
@@ -11,9 +19,9 @@ def main(global_config, **settings):
         settings['config'] = Config(config_file)
 
     config = Configurator(root_factory=Root, settings=settings)
-    config.add_view('demoapp.views.default_view',
-                    context='demoapp:resources.Root',
-                    renderer='json')
+    for index, (url, view, renderer) in enumerate(URLS):
+        name = 'view%d' % index
+        config.add_route(name, url, renderer=renderer, view=view)
 
     config.add_static_view('static', 'demoapp:static', cache_max_age=3600)
     return config.make_wsgi_app()
