38272f20dfa5e050bf4bb3685e25ef991f50f5c9
Tarek Ziadé <tarek@mozilla.com>
2011-10-07 15:21:22 +0200

    hooked the config file and demonstrate how it can be used in a view

--- demoapp/__init__.py
+++ demoapp/__init__.py
@@ -1,13 +1,19 @@
 from pyramid.config import Configurator
 from demoapp.resources import Root
+from demoapp.config import Config
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    config_file = settings.get('configuration')
+    if config_file:
+        settings['config'] = Config(config_file)
+
     config = Configurator(root_factory=Root, settings=settings)
-    config.add_view('demoapp.views.my_view',
+    config.add_view('demoapp.views.default_view',
                     context='demoapp:resources.Root',
-                    renderer='demoapp:templates/mytemplate.pt')
+                    renderer='json')
+
     config.add_static_view('static', 'demoapp:static', cache_max_age=3600)
     return config.make_wsgi_app()
--- demoapp/views.py
+++ demoapp/views.py
@@ -1,2 +1,8 @@
-def my_view(request):
-    return {'project':'demoapp'}
+
+def get_config(request):
+    return request.registry.settings.get('config')
+
+
+def default_view(request):
+    config = get_config(request)
+    return config.get_map()
