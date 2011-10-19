98821353052d1a84cfe06f8779b568017a01dc3b
Tarek Ziadé <tarek@mozilla.com>
2011-10-19 11:32:49 +0200

    now using include()

--- demoapp/__init__.py
+++ demoapp/__init__.py
@@ -1,4 +1,40 @@
 import demoapp
-from cornice import make_main
+#from cornice import make_main
+import os
 
-main = make_main(demoapp)
+from webob.exc import HTTPNotFound, HTTPMethodNotAllowed
+
+from pyramid.config import Configurator
+from pyramid.authorization import ACLAuthorizationPolicy
+from pyramid.events import BeforeRender
+
+from cornice.resources import Root
+from cornice.config import Config
+from cornice import util
+from cornice.service import Service, get_service     # NOQA
+
+
+def main(global_config, **settings):
+    config_file = global_config['__file__']
+    config_file = os.path.abspath(
+                    os.path.normpath(
+                    os.path.expandvars(
+                        os.path.expanduser(
+                        config_file))))
+
+    settings['config'] = config = Config(config_file)
+    conf_dir, _ = os.path.split(config_file)
+
+    authz_policy = ACLAuthorizationPolicy()
+    config = Configurator(root_factory=Root, settings=settings,
+                            authorization_policy=authz_policy)
+
+    # add auth via repoze.who
+    # eventually the app will have to do this explicitly
+    config.include("cornice.auth.whoauth")
+
+    # adds cornice
+    config.include("cornice")
+
+    config.scan("demoapp.views")
+    return config.make_wsgi_app()
